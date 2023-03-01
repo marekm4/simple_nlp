61a34b85bad3e2a64691c7e827744d9a038fb3ba
Marek Michalik <marekmichalik43@gmail.com>
2023-03-01 02:12:37 +0100

    Rename library

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.simple_nlp.preprocessing import remove_multiple_spaces
+from src.simple_nlp_library.preprocessing import remove_multiple_spaces
 
 
 class TestPreprocessing(unittest.TestCase):
