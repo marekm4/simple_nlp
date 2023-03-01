1e1b9a10fd017916195f6ce0f66c0cd270c192f3
Marek Michalik <marekmichalik43@gmail.com>
2023-03-01 03:22:09 +0100

    Add fast semantic token extractions

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Simple NLP library
 ```
 python3 -m pip install --upgrade simple_nlp_library
 ```
+
+## usage
+```python
+from src.simple_nlp_library import preprocessing
+
+
+```
--- src/simple_nlp_library/preprocessing.py
+++ src/simple_nlp_library/preprocessing.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 
 def remove_multiple_spaces(text: str) -> str:
@@ -7,3 +8,15 @@ def remove_multiple_spaces(text: str) -> str:
 
 def leave_only_uncased_letters(text: str) -> str:
     return re.sub('[^a-z ]+', '', text.lower())
+
+
+def remove_stop_words(tokens: List[str]) -> List[str]:
+    stop_words = ['the']
+    return [token for token in tokens if token not in dict.fromkeys(stop_words)]
+
+
+def retrieve_semantic_tokens(text: str) -> List[str]:
+    text = remove_multiple_spaces(text)
+    text = leave_only_uncased_letters(text)
+    tokens = remove_stop_words(text.split())
+    return tokens
